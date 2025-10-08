c18861256f82616bf3a5360e899d3ce0b674bcf7
Mick West <mick@mickwest.com>
2025-10-07 21:26:42 -0700

    Don't copy uneeded files in Docker builds

--- webpackCopyPatterns.js
+++ webpackCopyPatterns.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const InstallPaths = require('./config/config-install');
 
-module.exports = [
+// In Docker development mode, sitrecServer is served by Apache via proxy
+// So we don't need to copy it to the webpack output directory
+const isDockerDev = process.env.NODE_ENV === 'development' && InstallPaths.dev_path === '/var/www/html';
+
+const patterns = [
     // copies the data directory
     { from: "data", to: "./data"},
-    { from: "sitrecServer", to: "./sitrecServer"},
 
     // copy the shared.env file, renaming it to shared.env.php to prevent direct access
     // combined with the initial <?php tag, this will prevent the file from being served
@@ -15,13 +18,20 @@ module.exports = [
             return Buffer.from(updatedContent);
         },},
 
-    // the php configuration file is copied into the server folder
-    // for deployment
-    { from: "./config/config.php", to: "./sitrecServer/config.php",},
-
     // Web worker source code needs to be loaded at run time
     // so we just copy it over
     // This is currently not used
     { from: "./src/workers/*.js", to:""},
     { from: "./src/PixelFilters.js", to:"./src"},
 ];
+
+// Only copy sitrecServer and config.php in production or non-Docker environments
+if (!isDockerDev) {
+    patterns.push(
+        { from: "sitrecServer", to: "./sitrecServer"},
+        // the php configuration file is copied into the server folder for deployment
+        { from: "./config/config.php", to: "./sitrecServer/config.php",}
+    );
+}
+
+module.exports = patterns;
